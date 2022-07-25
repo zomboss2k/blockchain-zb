c3c7667bec4709f82449554b98664d15ed54a771
zomboss <zomboss.zed@gmail.com>
2022-07-25 14:36:23 +0700

    commit

---
+++ src/config/data.js
@@ -0,0 +1,18 @@
+export const chartDays = [
+  {
+    label: "24 Giờ",
+    value: 1,
+  },
+  {
+    label: "30 Ngày",
+    value: 30,
+  },
+  {
+    label: "3 Tháng",
+    value: 90,
+  },
+  {
+    label: "1 Năm",
+    value: 365,
+  },
+];
